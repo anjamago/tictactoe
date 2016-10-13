812c1ee27d426407c4f55b31d3caa966d40f4ca5
andy martinez <anjamago@gmail.com>
2016-10-13 01:52:44 -0500

    el diseño esta feo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 
 var player_titactoe =[];
-
+var count =0;
 var    jugadas={
     'jugador1':[],
     'jugador2':[]
 };
+var jugar = true;
 var rules = [
   //horizontales
             [0,3,6],
@@ -25,12 +26,44 @@ var Tictactoe = function(){
      jugador2:"maquina",
      parent:null,
     //metodos
-    createElements:function(element){
+    createElements:function(element,key){
       var element_li = document.createElement('li');
       element_li.setAttribute('class','tacbutom');
+      element_li.setAttribute('id',key);
       element.appendChild(element_li);
       this.parent = element_li;
 
+    },
+    rulesPlay:function(){
+      var jugador1 = jugadas.jugador1;
+      var jugador2 = jugadas.jugador2;
+      var x = 0;
+      var o = 0;
+      for(var key in rules)
+      {
+        regla = rules[key];
+
+        for(var j in regla)
+        {
+          var i = regla[j];
+
+          if(jugador1[i] === 'X'){
+            x=x+1;
+
+          }
+          if(jugador2[i] === 'O'){
+            o++;
+          }
+
+        }
+        if(x < 3 && o<3)
+        {
+          x =0;
+          o =0;
+        }
+      }
+
+      return x>o?'X': x<o?'O':'';
     },
     loadPlayer:function(){
       var name_user  = document.querySelector('#name_user');
@@ -47,21 +80,38 @@ var Tictactoe = function(){
 
       var oponente = jugadas.jugador1;
       var maquina = jugadas.jugador2;
+      var position;
       var span = document.createElement('span');
 
 
       var i=0;
       while(i < 1){
         var key = Math.round(Math.random()*(8-1)+1);
-        console.log(key);
+
           if(oponente[key] !=='X' ){
             if(maquina[key]!=='O')
             {
+              position = document.getElementById(key);
               jugadas.jugador2[key]= 'O';
               span.style.color = '#FF4F00';
               span.innerHTML='O';
-              html=player_titactoe[key].parent;
-              html.appendChild(span);
+              span.style.position ='relative';
+              span.style.float = 'right';
+              span.style.marginTop ='30%';
+              span.style.marginRight ='40%';
+              var ganador = this.rulesPlay();
+
+              if(ganador === ''){
+                position.appendChild(span);
+              }else{
+                if(ganador ==='O')
+                {
+                    position.appendChild(span);
+                }
+                  user =  ganador ==='X'?this.jugador1:ganador==='O'?this.jugador2:'';
+                  document.querySelector('section #ganador').innerHTML = 'Aganador: '+user +'<br>';
+                  jugar=false;
+              }
               //.appendChild(span);
               return ;
 
@@ -75,25 +125,40 @@ var Tictactoe = function(){
 
     },
     getPlay:function(key,constuct){
-      var position =key;
-      var self =constuct;
-      var span = document.createElement('span');
-      this.parent.addEventListener('click',function(){
 
-          console.log(self.jugador1);
-          jugadas.jugador1[position]= 'X';
-          span.style.color = '#5C51E5';
-          span.innerHTML='X';
+        var position =key;
+        var self =constuct;
+        var span = document.createElement('span');
 
-            self.getMachine(this);
+        this.parent.addEventListener('click',function(){
 
+            if(jugar){
 
+                jugadas.jugador1[position]= 'X';
+                span.style.color = '#5C51E5';
+                span.style.position ='relative';
+                span.style.float = 'right';
+                span.style.marginTop ='30%';
+                span.style.marginRight ='40%';
+                span.innerHTML='X';
+                this.appendChild(span);
+                count++;
 
-        this.appendChild(span);
+                if(count <5){
+                  self.getMachine(this);
+                }else if(count === 5 && self.rulesPlay() === ''){
+                  document.querySelector('section #ganador').innerHTML='Nadien gana';
+                }else{
+                  ganador = self.rulesPlay();
 
+                  user =  ganador ==='X'?self.jugador1:ganador==='O'?self.jugador2:'';
+                  document.querySelector('section #ganador').innerHTML = 'Aganador: '+user +'<br>';
+                  jugar=false;
 
+                }
+              }
+        });
 
-      });
 
     },
     loadSetting:function(constuct){
@@ -117,11 +182,35 @@ var Tictactoe = function(){
 //eventos
 document.addEventListener('DOMContentLoaded',function(event){
 
-  var player = document.querySelector('#load_player');
+var player = document.querySelector('#load_player');
 player.addEventListener('click',function(){
   var user_name = Tictactoe();
   user_name.loadPlayer();
 });
+
+//reset
+var reset = document.querySelector('#reset');
+reset.addEventListener('click',function(){
+   jugar= true;
+   count =0;
+
+   jugadas={
+      'jugador1':[],
+      'jugador2':[]
+  };
+  var reset = Tictactoe();
+  var hijos = document.querySelectorAll('#tictactoe .tacbutom');
+
+  var i =0;
+  for( i;i< hijos.length;i++){
+      document.querySelector('section #tictactoe').removeChild(hijos[i]);
+  }
+  document.querySelector('section #ganador').innerHTML ='';
+  player_titactoe=[];
+  reset.loadPlayer();
+
+});
+
 
 
 
